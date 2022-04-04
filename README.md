11aeebd094d08e64446f67fb52d69074e5493186
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-04-03 22:46:10 -0400

    Flesh out basic HTML pages - Welcome page & results page. CSS formatting for 2 column layout on results page. Commited file structure & starter JS file.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Library of Congress Search Engine
+
+A quick and easy way to search one of the world's greatest archives of science, literature, music and more.
+
+# Description
+
+# Acceptance Criteria
+
+# Useage
---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+var searchResults = document.querySelector("#searchResults");
+
+var searchResults = function (event) {
+  for (i = 0; i < 15; i++) {
+    var displayResults = document.createElement("tr");
+    displayResults.setAttribute("class", "row");
+    displayResults.innerHTML =
+      // ${*** INSERT RESULTS HERE?}
+
+      `
+      <div class="searchResultsCard">
+      <div class="card-header">
+      <h2>This is an example search result</h2>
+      </div>
+      <div class="card-body">
+      <p>Example search results Example search results Example search results Example search results Example search results Example search results Example search results Example search results  </p>
+      </div>
+      </div>
+    `;
+    searchResults.append(displayResults);
+  }
+};
