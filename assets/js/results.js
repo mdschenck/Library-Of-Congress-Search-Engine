2ee25a19dc38aa314972b7ec99285a2b3372d3f7
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-04-05 21:19:37 -0400

    adding working js html & css

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,22 +1,49 @@
+var searchInputEl = document.querySelector("#resultsPgSearchInput");
+var formatEL = document.querySelector("#format");
 var searchResults = document.querySelector("#searchResults");
-
-var searchResults = function (event) {
-  for (i = 0; i < 15; i++) {
-    var displayResults = document.createElement("tr");
-    displayResults.setAttribute("class", "row");
-    displayResults.innerHTML =
-      // ${*** INSERT RESULTS HERE?}
-
-      `
-        <div class="searchResultsCard">
-        <div class="card-header">
-        <h2>This is an example search result</h2>
-        </div>
-        <div class="card-body">
-        <p>Example search results Example search results Example search results Example search results Example search results Example search results Example search results Example search results  </p>
-        </div>
-        </div>
-      `;
-    searchResults.append(displayResults);
+var paramsString = document.location.search;
+var searchParams = new URLSearchParams(paramsString);
+var searchQ = searchParams.get("q");
+var searchURL = "./search-results.html?q=";
+var searchString = searchInputEl.value.replace(" ", "+");
+function init() {
+  searchString = searchString.trim();
+  getLibraryAPI();
+}
+var renderResults = function (data) {
+  var results = data["results"];
+  var resultHeaderEL = document.createElement("div");
+  resultHeaderEL.innerHTML = `<h2 color="white">Showing Results for "${searchQ}"</h2>`;
+  searchResults.appendChild(resultHeaderEL);
+  for (var i = 0; i < results.length; i++) {
+    var title = results[i].title;
+    var date = results[i].timestamp;
+    var subject = results[i].subject;
+    var description = results[i].description;
+    var readMore = results[i].id;
+    var cardEl = document.createElement("div");
+    cardEl.classList.add("card");
+    cardEl.innerHTML = `<div class="card-header"><h2>${title}</h2></div>
+      <div><h3>${subject} - ${date}</h3></div>
+      <div class="card-body"><p> ${description} </p></div>
+      <div><a href="${readMore}" target="top">Read More</a></div>`;
+    searchResults.appendChild(cardEl);
   }
 };
+function getLibraryAPI() {
+  var apiUrl = "https://www.loc.gov/search/?" + searchParams + "&fo=json";
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        renderResults(data);
+      });
+    } else {
+      document.location.replace("./index.html");
+    }
+  });
+}
+function listen() {
+  searchFormEl.addEventListener("submit", getLibraryApi);
+}
+init();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 var searchFormEl = document.querySelector("#searchForm");
 var searchInput = document.querySelector("#searchInput");
+var formatEL = document.querySelector("#format");
 
-function addEventListeners() {
-  searchFormEl.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var searchTerm = searchFormEl.value;
-    var format = formats.value;
-
-    document.location.replace(
-      "/results.html?q=" + searchTerm + "&format=" + format
-    );
-  });
-}
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var searchURL = "./results.html?q=";
+  var searchString = searchInputEl.value.replace(" ", "+");
+  searchString = searchString.trim();
+  console.log(searchString);
+  searchURL = searchURL + searchString;
+  searchURL = searchURL + "&format=" + formatEL.value;
+  console.log(searchURL);
+  document.location.replace(searchURL);
+};
+searchFormEl.addEventListener("submit", formSubmitHandler);
